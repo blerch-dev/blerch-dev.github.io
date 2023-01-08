f88ef4df3585d76d95440a30c86414f5202bcf69
blerch-dev <blerch.dev@gmail.com>
2023-01-08 01:04:00 -0600

    Debugging Page

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
+console.log(window.location.pathname, window.location.pathname === '/chat-speaker');
 if(window.location.pathname === '/chat-speaker')
     window.location.href = "https://github.com/blerch-dev/chat-speaker/releases/tag/v0.1.0"
