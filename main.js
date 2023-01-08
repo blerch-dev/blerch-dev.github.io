364d7012cf0424918d4e63ff195ae62bcc40aa9d
blerch-dev <blerch.dev@gmail.com>
2023-01-08 01:05:16 -0600

    Fixed Routes

--- main.js
+++ main.js
@@ -1,3 +1,2 @@
-console.log(window.location.pathname, window.location.pathname === '/chat-speaker');
-if(window.location.pathname === '/chat-speaker')
+if(window.location.pathname === '/chat-speaker/')
     window.location.href = "https://github.com/blerch-dev/chat-speaker/releases/tag/v0.1.0"
